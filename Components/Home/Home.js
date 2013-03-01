54f9fe8c1557093fd5a36cb48bba3425f20fbaa4
Justin Tulloss <justin@rd.io>
2013-03-01 13:10:53 -0800

    A blog that loads a component but not the template or style

---
+++ Components/Home/Home.js
@@ -0,0 +1,4 @@
+(function() {
+    "use strict";
+    R.Component.create('Home', {});
+}).call(this);
--- blog/init.js
+++ blog/init.js
@@ -1,6 +1,15 @@
 (function() {
     "use strict";
     R.init = function() {
-        document.body.innerHTML = marked("**Welcome to our blog!**");
+        R.Services.start();
+
+        R.Services.ready('Loader', function() {
+            R.loader.load(['Home'], function() {
+                var home = new R.Components.Home();
+                home.render(function() {
+                    $('body').append(home.el);
+                });
+            });
+        });
     };
 }).call(this);
