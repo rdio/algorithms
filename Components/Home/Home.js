3f0a80a8195dbe5a57a10b95388d82149cb1f8ce
Justin Tulloss <justin@rd.io>
2013-03-01 17:17:40 -0800

    Move metadata to published_posts.json

--- Components/Home/Home.js
+++ Components/Home/Home.js
@@ -1,4 +1,10 @@
 (function() {
     "use strict";
-    R.Component.create('Home', {});
+    var PostsCollection = Backbone.Collection.extend({
+        shouldFetch: true,
+        url: '/posts/published_posts.json'
+    });
+    R.Component.create('Home', {
+        modelClass: PostsCollection
+    });
 }).call(this);
